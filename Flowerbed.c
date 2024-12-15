e88dd72f90fb5f16203e3a1d3266d990789399bb
Vítor Castilhos <vitorcastilhos1@hotmail.com>
2024-12-14 23:02:27 -0300

    improved readability of the code

--- Flowerbed.c
+++ Flowerbed.c
@@ -12,35 +12,35 @@ printf("%d",CanPlaceFlowers(flowerbed,size,n));
 }
 
 bool CanPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
-    int counter = 0;
-    int flag = 0;
-    if ( flowerbedSize == 1 ) {                                             //checa se o array tem tamanho 1
-        if( flowerbed[0] == 0 )                                             
-        counter++;
+    int canPlant = 0;
+    int plantedLast = 0;
+    if ( flowerbedSize == 1 ) {                                                                  //checa se o array tem tamanho 1
+        if( flowerbed[0] == 0 )                                                                  
+     canPlant++;
     }
-    else for ( int i = 0 ; i < flowerbedSize ; i++ ) {                      //se for maior que 1,passa por todo o array
-        if ( flag == 0 ) {
-                if ( i == 0 ) {                                             //checa se é o primeiro termo
-                    if ( flowerbed[i] == 0 && flowerbed[i+1] == 0 ) {       //se o primeiro termo for vazio
-                        counter++;                                      
-                        flag++;                                         
+    else for ( int i = 0 ; i < flowerbedSize ; i++ ) {                                           //se for maior que 1,passa por todo o array
+        if ( plantedLast == 0 ) {
+                if ( i == 0 ) {                                                                  //checa se é o primeiro termo
+                    if ( flowerbed[i] == 0 && flowerbed[i+1] == 0 ) {                            //se o primeiro termo for vazio
+                        canPlant++;                                      
+                        plantedLast++;                                                              
                         }
                     }
-                else if ( i == flowerbedSize-1 ) {                          //checa se é o ultimo
+                else if ( i == flowerbedSize - 1 ) {                                             //checa se é o ultimo
                     if ( flowerbed[i-1] == 0 && flowerbed[i] == 0 ) {       
-                        counter++;                                      
-                        flag++;
+                        canPlant++;                                      
+                        plantedLast++;
                         }
                     }
                     else if ( flowerbed[i-1] == 0 && flowerbed[i] == 0 && flowerbed[i+1] == 0 ) {
-                        counter++;                                          //testa se o anterior, atual e proximo são
-                        flag++;                                             //vazios, se sim, aumenta o contador e
-                    }                                                       //ativa a flag
+                        canPlant++;                                                              //testa se o anterior, atual e proximo são
+                        plantedLast++;                                                           //vazios, se sim, aumenta o contador e
+                    }                                                                            //ativa a plantedLast
             }
             else
-                flag = 0;
+                plantedLast = 0;
     }
-    if ( n <= counter )                                               
+    if ( n <= canPlant )                                               
         return true;                                                   
     else
         return false;
